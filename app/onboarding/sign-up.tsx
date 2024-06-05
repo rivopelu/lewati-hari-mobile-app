3ae07bf60b11fdd1c158e4e9c1c81b0f0101823e
rivo <sakshi.kalani@floodouts.com>
2024-06-05 20:10:53 +0800

    FEAT:setup screen register

--- app/onboarding/sign-up.tsx
+++ app/onboarding/sign-up.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 
 import MainProviders from '@/components/layouts/MainProviders';
-import { StatusBar, View, Text } from 'react-native';
+import { StatusBar, View, Text, ScrollView } from 'react-native';
 import { HeaderGradient } from '@/components/atoms/HeaderGradient';
 import { Spacer } from '@/components/atoms/Spacer';
 import { Container } from '@/components/atoms/Container';
@@ -20,80 +20,84 @@ export default function SignUpScreen() {
     <MainProviders>
       <HeaderGradient />
       <StatusBar barStyle="dark-content" />
-      <Spacer y={48} />
-      <View style={{ flex: 1 }}>
-        <View style={{ justifyContent: 'space-around', flex: 1 }}>
-          <Container>
-            <View style={{ alignItems: 'center', justifyContent: 'center', gap: 24 }}>
-              <View
-                style={{
-                  width: 160,
-                  justifyContent: 'flex-start',
-                  alignContent: 'flex-start',
-                }}>
-                <Image
-                  alt="lewati hari"
-                  source={require('@/assets/logo/lg_brand.png')}
-                  style={{ height: 32, objectFit: 'contain', width: 'auto' }}
-                />
-              </View>
-              <Text style={{ fontSize: 24, width: '100%', fontWeight: '600', textAlign: 'center' }}>
-                Yuk, masukin informasi kamu buat daftar
-              </Text>
-            </View>
-          </Container>
-          <Container>
-            <View style={{ gap: 16 }}>
-              <View>
-                <InputText
-                  required
-                  placeholder="Masukin nama kamu"
-                  label="Nama"
-                  onChangeText={(e) => console.log(e)}
-                />
-                <InputText
-                  keyboardType="email-address"
-                  required
-                  placeholder="Masukan email"
-                  label="Email"
-                  onChangeText={(e) => console.log(e)}
-                />
-                <InputText
-                  secureTextEntry={!showPassword}
-                  required
-                  placeholder="Masukan kata sandi"
-                  label="Kata sandi"
-                  onChangeText={(e) => console.log(e)}
-                />
-                <InputText
-                  secureTextEntry={!showPassword}
-                  required
-                  placeholder="Masukin kata sandi sekali lagi"
-                  label="Konfirmasi kata sandi"
-                  onChangeText={(e) => console.log(e)}
-                />
-              </View>
-              <MainCheckbox
-                label="Tampilin kata sandi"
-                checked={showPassword}
-                onChange={(e) => setShowPassword(e)}
-              />
-              <Btn text="MASUK" />
+      <Spacer y={100} />
+      <ScrollView>
+        <View style={{ flex: 1 }}>
+          <View style={{ justifyContent: 'space-around', flex: 1, gap: 32 }}>
+            <Container>
               <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                <View style={{ flexDirection: 'row', gap: 7 }}>
-                  <Text>Udah punya akun ?</Text>
-                  <Link href="/onboarding/sign-in" style={{ padding: 0, margin: 0 }}>
-                    <Text style={{ color: Colors.primary.main }}>Masuk disini</Text>
-                  </Link>
+                <View
+                  style={{
+                    width: 160,
+                    justifyContent: 'flex-start',
+                    alignContent: 'flex-start',
+                  }}>
+                  <Image
+                    alt="lewati hari"
+                    source={require('@/assets/logo/lg_brand.png')}
+                    style={{ height: 32, objectFit: 'contain', width: 'auto' }}
+                  />
+                </View>
+                <Text
+                  style={{ fontSize: 24, width: '100%', fontWeight: '600', textAlign: 'center' }}>
+                  Yuk, masukin informasi kamu buat daftar
+                </Text>
+              </View>
+            </Container>
+            <Container>
+              <View style={{ gap: 16 }}>
+                <View>
+                  <InputText
+                    required
+                    placeholder="Masukin nama kamu"
+                    label="Nama"
+                    onChangeText={(e) => console.log(e)}
+                  />
+                  <InputText
+                    keyboardType="email-address"
+                    required
+                    placeholder="Masukan email"
+                    label="Email"
+                    onChangeText={(e) => console.log(e)}
+                  />
+                  <InputText
+                    secureTextEntry={!showPassword}
+                    required
+                    placeholder="Masukan kata sandi"
+                    label="Kata sandi"
+                    onChangeText={(e) => console.log(e)}
+                  />
+                  <InputText
+                    secureTextEntry={!showPassword}
+                    required
+                    placeholder="Masukin kata sandi sekali lagi"
+                    label="Konfirmasi kata sandi"
+                    onChangeText={(e) => console.log(e)}
+                  />
+                </View>
+                <MainCheckbox
+                  label="Tampilin kata sandi"
+                  checked={showPassword}
+                  onChange={(e) => setShowPassword(e)}
+                />
+                <Btn text="MASUK" />
+                <Btn text="MASUK DENGAN GOOGLE" />
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                  <View style={{ flexDirection: 'row', gap: 7 }}>
+                    <Text>Udah punya akun ?</Text>
+                    <Link href="/onboarding/sign-in" style={{ padding: 0, margin: 0 }}>
+                      <Text style={{ color: Colors.primary.main }}>Masuk disini</Text>
+                    </Link>
+                  </View>
                 </View>
               </View>
-            </View>
-          </Container>
-          <Container>
-            <Text style={{ textAlign: 'center', color: Colors.system.textGray }}>V 0.1.0</Text>
-          </Container>
+            </Container>
+            <Container>
+              <Text style={{ textAlign: 'center', color: Colors.system.textGray }}>V 0.1.0</Text>
+            </Container>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </MainProviders>
   );
 }
